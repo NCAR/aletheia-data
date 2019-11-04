8605ac38b0796e5223627f4e1686d1d45e1cc02d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-11-04 11:21:39 -0700

    Update docstring

--- aletheia_data/core.py
+++ aletheia_data/core.py
@@ -14,6 +14,71 @@ from .utils import infer_protocol_options
 
 
 def create(path, base_url, version=None, version_dev='master', env=None, registry=None, urls=None):
+    """
+    Create a new :class:`~aletheia_data.AletheiaPooch` with sensible defaults to fetch data files.
+    If a version string is given, the Pooch will be versioned, meaning that the local
+    storage folder and the base URL depend on the project version. This is necessary
+    if your users have multiple versions of your library installed (using virtual
+    environments) and you updated the data files between versions. Otherwise, every time
+    a user switches environments would trigger a re-download of the data. The version
+    string will be appended to the local storage path (for example,
+    ``~/.mypooch/cache/v0.1``) and inserted into the base URL (for example,
+    ``https://github.com/fatiando/pooch/raw/v0.1/data``). If the version string contains
+    ``+XX.XXXXX``, it will be interpreted as a development version.
+
+    Parameters
+    ----------
+    path : str, PathLike, list or tuple
+        The path to the local data storage folder. If this is a list or tuple, we'll
+        join the parts with the appropriate separator. The *version* will be appended to
+        the end of this path. Use :func:`pooch.os_cache` for a sensible default.
+    base_url : str
+        Base URL for the remote data source. All requests will be made relative to this
+        URL. The string should have a ``{version}`` formatting mark in it. We will call
+        ``.format(version=version)`` on this string. If the URL is a directory path, it
+        must end in a ``'/'`` because we will not include it.
+    version : str or None
+        The version string for your project. Should be PEP440 compatible. If None is
+        given, will not attempt to format *base_url* and no subfolder will be appended
+        to *path*.
+    version_dev : str
+        The name used for the development version of a project. If your data is hosted
+        on Github (and *base_url* is a Github raw link), then ``"master"`` is a good
+        choice (default). Ignored if *version* is None.
+    env : str or None
+        An environment variable that can be used to overwrite *path*. This allows users
+        to control where they want the data to be stored. We'll append *version* to the
+        end of this value as well.
+    registry : dict or None
+        A record of the files that are managed by this Pooch. Keys should be the file
+        names and the values should be their SHA256 hashes. Only files in the registry
+        can be fetched from the local storage. Files in subdirectories of *path* **must
+        use Unix-style separators** (``'/'``) even on Windows.
+    urls : dict or None
+        Custom URLs for downloading individual files in the registry. A dictionary with
+        the file names as keys and the custom URLs as values. Not all files in
+        *registry* need an entry in *urls*. If a file has an entry in *urls*, the
+        *base_url* will be ignored when downloading it in favor of ``urls[fname]``.
+    Returns
+    -------
+    pooch : :class:`~aletheia_data.AletheiaPooch`
+        The :class:`~aletheia_data.AletheiaPooch` initialized with the given arguments.
+    Examples
+    --------
+    Create a :class:`~aletheia_data.AletheiaPooch` for a release (v0.1):
+
+     >>> from aletheia_data import create
+     >>> p = create(path="myproject",
+     ...              base_url="ftp://ftp.cgd.ucar.edu/archive/aletheia-data/tutorial-data/",
+     ...              version="v0.1",
+     ...              registry={'rasm.nc': '28498798c1934277268ef806112c001a8281b59c18228a17797df38defad8dfb'})
+     >>> print(p.path.parts)  # The path is a pathlib.Path
+     ('/', 'Users', 'abanihi', '.aletheia', 'data', 'v0.1')
+     >>> print(p.base_url)
+     ftp://ftp.cgd.ucar.edu/archive/aletheia-data/tutorial-data/
+     >>> print(p.registry)
+     {'rasm.nc': '28498798c1934277268ef806112c001a8281b59c18228a17797df38defad8dfb'}
+    """
 
     if isinstance(path, (list, tuple)):
         path = os.path.join(*path)
